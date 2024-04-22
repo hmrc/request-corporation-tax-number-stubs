65557cefef65d34b89f1a21dd7437598d37ddddc
Adam George <151747054+adageorg@users.noreply.github.com>
2024-04-22 13:50:54 +0100

    DDCE-5146: Updating to Play 3.0 Also committing copyright header updates brought about by building the project.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.{ModuleID, _}
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.21.0"
+  private val bootstrapVersion = "8.5.0"
 
   private val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "domain" % "8.3.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc" %% "domain-play-30"            % "9.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
 
     "com.vladsch.flexmark"   %  "flexmark-all"            % "0.64.8",
-    "org.scalatest"          %% "scalatest"               % "3.2.16",
-    "org.scalatestplus"      %% "mockito-4-11"            % "3.2.16.0",
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion
+    "org.scalatest"          %% "scalatest"               % "3.2.18",
+    "org.scalatestplus"      %% "mockito-5-10"            % "3.2.18.0",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
